c0643ac25bccde640e269b2696f6a61c12c42c80
agiv <webdev111222@gmail.com>
2026-07-30 01:48:35 -0700

    Remove unused MongoDB connection

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('MongoDB connection error:', error.message);
-  });
 
 app.use(cors({
   origin: process.env.FRONTEND_URL
